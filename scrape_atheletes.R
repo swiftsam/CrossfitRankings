ba89a28bd98c97ea0b8eb5e637fcc46873f6bf17
Sam Swift <samswift@gmail.com>
2014-03-25 15:58:43 -0400

    scrape atheletes is working

---
+++ scrape_atheletes.R
@@ -0,0 +1,54 @@
+
+library(httr)
+library(XML)
+library(data.table)
+library(reshape2)
+library(stringr)
+
+load("data/leaderboard.RData")
+
+for(i in 1:nrow(scores.bind)){
+  url <- scores.bind[i, url]
+  url.vector <- strsplit(url,"/")[[1]]
+  id  <- url.vector[length(url.vector)]
+  resp <- GET(url)  
+  if (resp$status_code==200){
+    page  <- htmlParse(resp)
+    
+    labels <- xpathSApply(page, "//div[@class='profile-details']/dl/dt", xmlValue)
+    demo   <- xpathSApply(page, "//div[@class='profile-details']/dl/dd", xmlValue)
+    stats  <- xpathSApply(page, "//div[@class='profile-stats']//td",     xmlValue)
+    
+    athlete <- data.table("id"        = id,
+                          "region"    = paste(demo[which(labels == "Region:")],"",sep=""),
+                          "team"      = paste(demo[which(labels == "Team:")],"", sep=""),
+                          "affiliate" = paste(demo[which(labels == "Affiliate:")],"",sep=""),
+                          "gender"    = paste(demo[which(labels == "Gender:")],"",sep=""),
+                          "age"       = paste(demo[which(labels == "Age:")],"",sep=""),
+                          "height"    = paste(demo[which(labels == "Height:")],"",sep=""),
+                          "weight"    = paste(demo[which(labels == "Weight:")],"",sep=""),
+                          "fran"      = stats[2],
+                          "helen"     = stats[4],
+                          "grace"     = stats[6],
+                          "filthy50"  = stats[8],
+                          "fgonebad"  = stats[10],
+                          "run400"    = stats[12],
+                          "run5k"     = stats[14],
+                          "candj"     = stats[16],
+                          "snatch"    = stats[18],
+                          "deadlift"  = stats[20],
+                          "backsq"    = stats[22],
+                          "pullups"   = stats[24])
+    if(exists("athletes")){
+      athletes <- rbind(athletes, athlete)
+    } else {
+      athletes <- athlete
+    }
+    if(i%%100 == 0){
+      save(athletes, file=paste("data/athletes_",i,".RData", sep=""))      
+    }
+  }
+}
+
+save(athletes, file="data/athletes.RData")
+
