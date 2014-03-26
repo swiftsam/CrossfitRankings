cc70a3089e4677598c308c992202f27d8b695b51
Sam Swift <samswift@gmail.com>
2014-03-26 00:34:19 -0400

    making it easier to split athelete scraping across machines

--- scrape_athletes.R
+++ scrape_athletes.R
@@ -5,56 +5,78 @@ library(data.table)
 library(reshape2)
 library(stringr)
 
-load("data/leaderboard.RData")
-
-urls <- sort(unique(scores.bind$url))
-
-for(i in 1:length(urls)){
-  url <- urls[i]
-  url.vector <- strsplit(url,"/")[[1]]
-  id  <- url.vector[length(url.vector)]
-  resp <- GET(url)  
-  if (resp$status_code==200){
-    page  <- htmlParse(resp)
-    
-    labels <- xpathSApply(page, "//div[@class='profile-details']/dl/dt", xmlValue)
-    demo   <- xpathSApply(page, "//div[@class='profile-details']/dl/dd", xmlValue)
-    stats  <- xpathSApply(page, "//div[@class='profile-stats']//td",     xmlValue)
-    
-    athlete <- data.table("id"        = id,
-                          "region"    = paste(demo[which(labels == "Region:")],"",sep=""),
-                          "team"      = paste(demo[which(labels == "Team:")],"", sep=""),
-                          "affiliate" = paste(demo[which(labels == "Affiliate:")],"",sep=""),
-                          "gender"    = paste(demo[which(labels == "Gender:")],"",sep=""),
-                          "age"       = paste(demo[which(labels == "Age:")],"",sep=""),
-                          "height"    = paste(demo[which(labels == "Height:")],"",sep=""),
-                          "weight"    = paste(demo[which(labels == "Weight:")],"",sep=""),
-                          "fran"      = stats[2],
-                          "helen"     = stats[4],
-                          "grace"     = stats[6],
-                          "filthy50"  = stats[8],
-                          "fgonebad"  = stats[10],
-                          "run400"    = stats[12],
-                          "run5k"     = stats[14],
-                          "candj"     = stats[16],
-                          "snatch"    = stats[18],
-                          "deadlift"  = stats[20],
-                          "backsq"    = stats[22],
-                          "pullups"   = stats[24])
-    if(exists("athletes")){
-      athletes <- rbind(athletes, athlete)
-    } else {
-      athletes <- athlete
-    }
-    if(i%%100 == 0){
-      if(file.exists(paste("data/athletes_",i-2,".RData", sep=""))){
-        file.remove(paste("data/athletes_",i-2,".RData", sep=""))
-      }
-      message("done with ", i)
-      save(athletes, file=paste("data/athletes_",i,".RData", sep=""))      
-    }
-  }
+if(file.exists("data/leaderboard.RData")){
+  load("data/leaderboard.RData")  
+} else {
+  stop("Can't find leaderbord data")  
 }
 
-save(athletes, file="data/athletes.RData")
+if(file.exists("data/athletes.RData")){
+  load("data/athletes.RData")  
+}
 
+urls <- unique(scores.bind$url)
+ids <- sort(
+        as.numeric(
+          unlist(
+            lapply(
+              unique(scores.bind$url), 
+              function(url){ 
+                url.vector <- strsplit(url,"/")[[1]]
+                id <- url.vector[length(url.vector)]
+                return(id)
+        }))))
+
+ScrapeAthletes <- function(start.i = 1, 
+                           end.i   = length(ids)){
+  
+  # identify IDs in the desired range which are not already scraped
+  ids.loop <- ids[start.i:end.i]
+  if(exists("athletes")){
+    ids.loop <- setdiff(ids.loop, athletes$id)
+  }
+  
+  # loop through IDs we don't have yet
+  for(i in 1:length(ids.loop)){
+    id <- ids.loop[i]
+    resp <- GET(paste("http://games.crossfit.com/athlete/",id,sep=""))  
+    if (resp$status_code==200){
+      page  <- htmlParse(resp)
+      
+      labels <- xpathSApply(page, "//div[@class='profile-details']/dl/dt", xmlValue)
+      demo   <- xpathSApply(page, "//div[@class='profile-details']/dl/dd", xmlValue)
+      stats  <- xpathSApply(page, "//div[@class='profile-stats']//td",     xmlValue)
+      
+      athlete <- data.table("id"        = id,
+                            "region"    = paste(demo[which(labels == "Region:")],"",sep=""),
+                            "team"      = paste(demo[which(labels == "Team:")],"", sep=""),
+                            "affiliate" = paste(demo[which(labels == "Affiliate:")],"",sep=""),
+                            "gender"    = paste(demo[which(labels == "Gender:")],"",sep=""),
+                            "age"       = paste(demo[which(labels == "Age:")],"",sep=""),
+                            "height"    = paste(demo[which(labels == "Height:")],"",sep=""),
+                            "weight"    = paste(demo[which(labels == "Weight:")],"",sep=""),
+                            "fran"      = stats[2],
+                            "helen"     = stats[4],
+                            "grace"     = stats[6],
+                            "filthy50"  = stats[8],
+                            "fgonebad"  = stats[10],
+                            "run400"    = stats[12],
+                            "run5k"     = stats[14],
+                            "candj"     = stats[16],
+                            "snatch"    = stats[18],
+                            "deadlift"  = stats[20],
+                            "backsq"    = stats[22],
+                            "pullups"   = stats[24])
+      if(exists("athletes")){
+        athletes <- rbind(athletes, athlete)
+      } else {
+        athletes <- athlete
+      }
+      if(i%%100 == 0){
+        message("done with i:", i," id:",id)
+        save(athletes, file=paste("data/athletes.RData", sep=""))  
+      }
+    }
+  }
+  save(athletes, file=paste("data/athletes.RData", sep="")) 
+}
