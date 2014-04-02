f912c2b5e7d2a8d102b1d3d0560609d0ab6b5e3e
Sam Swift <samswift@gmail.com>
2014-04-02 16:03:03 -0400

    revised plots

--- plot_leaderboards.R
+++ plot_leaderboards.R
@@ -7,35 +7,39 @@ load("data/leaderboard.RData")
 
 ### Histograms by WOD
 ggplot(leaderboard[wod=="wod1",], aes(score)) +
-  geom_histogram(aes(y=..density..),binwidth=1, position="dodge")+
+  geom_histogram(binwidth=1)+
   scale_x_continuous(limits = c(0,475),
                      breaks = seq(0,475,25))+
-  scale_y_continuous(labels=percent) +
-  labs(x="14.1 Score",y="% of Athletes") +
+  labs(x="14.1 Score",y="# of Athletes") +
   theme_bw(base_size=18)
 
-
 ggplot(leaderboard[wod=="wod2",], aes(score)) +
-  geom_histogram(aes(y=..density..),binwidth=1, position="dodge")+
+  geom_histogram(binwidth=1)+
   scale_x_continuous(limits = c(0,425),
                      breaks = seq(0,425,25))+
-  scale_y_continuous(labels=percent) +
-  labs(x="14.2 Score",y="% of Athletes") +
+  labs(x="14.2 Score",y="# of Athletes") +
   theme_bw(base_size=18)
 
 ggplot(leaderboard[wod=="wod3",], aes(score)) +
-  geom_histogram(aes(y=..density..),binwidth=1, position="dodge")+
+  geom_histogram(binwidth=1)+
   scale_x_continuous(limits = c(0,200),
                      breaks = seq(0,200,10))+
-  scale_y_continuous(labels=percent) +
-  labs(x="14.3 Score",y="% of Athletes") +
+  labs(x="14.3 Score",y="# of Athletes") +
   theme_bw(base_size=18)
 
 
 ggplot(leaderboard[wod=="wod4",], aes(score)) +
-  geom_histogram(aes(y=..density..),binwidth=1, position="dodge")+
+  geom_histogram(binwidth=1)+
   scale_x_continuous(limits = c(60,280),
                      breaks = seq(0,280,10))+
-  scale_y_continuous(labels=percent) +
-  labs(x="14.4 Score",y="% of Athletes") +
+  labs(x="14.4 Score",y="# of Athletes") +
   theme_bw(base_size=18)
+
+ggplot(leaderboard[wod=="wod5",], aes(score)) +
+  geom_histogram(binwidth=1)+
+  scale_x_continuous(limits = c(420,3600),
+                     breaks = seq(420,3600, 120),
+                     labels = seq(7,60,2))+
+  labs(x="14.5 time (minutes)",y="# of Athletes") +
+  theme_bw(base_size=18)
+
