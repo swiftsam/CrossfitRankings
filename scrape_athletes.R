3ac15df5201e907eaae44a69a78ff14573215869
Sam Swift <swift@betterment.com>
2015-03-02 10:10:29 -0500

    better progress messaging at the beginning of scraping athletes

--- scrape_athletes.R
+++ scrape_athletes.R
@@ -13,25 +13,32 @@ source("scrape_fns.R")
 source("db_query.R")
 
 ScrapeAtheletesByYear <- function(year){
-  athletes.all <- QueryDB(paste0("SELECT DISTINCT athlete_id
+  athletes.year <- QueryDB(paste0("SELECT DISTINCT athlete_id
                                   FROM leaderboard
                                   WHERE year = ", year))
   
-  athletes.done <- QueryDB("SELECT athlete_id
-                            FROM athletes;")
+  athletes.db <- QueryDB("SELECT athlete_id
+                          FROM athletes;")
   
-  athletes <- athletes.all[!athlete_id %in% athletes.done[,athlete_id], athlete_id]
+  n.year    <- athletes.year[, length(unique(athlete_id))]
+  n.scraped <- athletes.year[athlete_id %in% athletes.db[, athlete_id], length(unique(athlete_id))]
+  n.todo    <- athletes.year[!athlete_id %in% athletes.db[, athlete_id], length(unique(athlete_id))]
+
+  message(Sys.time(), " ScapeAtheletes: ", n.year, " athletes competed in 20", year)
+  message(Sys.time(), " ScapeAtheletes: ", n.scraped, " atheletes in the DB")
+  message(Sys.time(), " ScapeAtheletes: ", n.todo, " atheletes left to scrape")
+  message(Sys.time(), " ScapeAtheletes: ", round((n.scraped / n.year)*100), "% complete")
   
-  message(Sys.time(), " starting with ", length(athletes), " athletes to scrape")
-  
-  foreach(i = 1:length(athletes)) %dopar% {
+  athletes.todo <- athletes.year[!athlete_id %in% athletes.db[, athlete_id], unique(athlete_id)]
+    
+  foreach(i = 1:length(athletes.todo)) %dopar% {
     
     db.con <- dbConnect(RMySQL::MySQL(), 
                         dbname   = "crossfit",
                         user     = "crossfit",
                         password = "",
                         host     = "127.0.0.1")
-    athlete_id <- athletes[i]
+    athlete_id <- athletes.todo[i]
     message(Sys.time(), " Athlete: ", athlete_id)
     
     athlete <- GetAthlete(athlete_id)
