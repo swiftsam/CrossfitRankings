9ea0967bb4eeb05c1f215f9e88162a3f5321b045
Sam Swift <swift@betterment.com>
2015-03-01 20:03:20 -0500

    scraping athletes by year so we can get the 2015s done first

--- scrape_athletes.R
+++ scrape_athletes.R
@@ -8,33 +8,35 @@ source("process_fns.R")
 source("scape_fns.R")
 source("db_query.R")
 
-athletes.all <- QueryDB("SELECT DISTINCT athlete_id
-                    FROM leaderboard;")
-
-
-athletes.done <- QueryDB("SELECT athlete_id
-                    FROM athletes;")
-
-athletes <- athletes.all[!athlete_id %in% athletes.done[,athlete_id], athlete_id]
-
-message(Sys.time(), " starting with ", length(athletes), " athletes to scrape")
-
-for(athlete_id in athletes){
-  message(Sys.time(), " Athlete: ", athlete_id)
+ScrapeAtheletesByYear <- function(year){
+  athletes.all <- QueryDB(paste0("SELECT DISTINCT athlete_id
+                                  FROM leaderboard
+                                  WHERE year = ", year))
   
+  athletes.done <- QueryDB("SELECT athlete_id
+                            FROM athletes;")
+  
+  athletes <- athletes.all[!athlete_id %in% athletes.done[,athlete_id], athlete_id]
+  
+  message(Sys.time(), " starting with ", length(athletes), " athletes to scrape")
+  
+  for(athlete_id in athletes){
+    message(Sys.time(), " Athlete: ", athlete_id)
+    
     athlete <- GetAthlete(athlete_id)
-  
-  if(!is.null(athlete)) {
-    # write new leaderboard records
-    db.con <- dbConnect(RMySQL::MySQL(), 
-                        dbname   = "crossfit",
-                        user     = "crossfit",
-                        password = "",
-                        host     = "127.0.0.1")
     
-    dbWriteTable(db.con, name = "athletes", value=athlete, row.names = F, append=TRUE)
-    
-    dbDisconnect(db.con)
-  }
+    if(!is.null(athlete)) {
+      # write new leaderboard records
+      db.con <- dbConnect(RMySQL::MySQL(), 
+                          dbname   = "crossfit",
+                          user     = "crossfit",
+                          password = "",
+                          host     = "127.0.0.1")
+      
+      dbWriteTable(db.con, name = "athletes", value=athlete, row.names = F, append=TRUE)
+      
+      dbDisconnect(db.con)
+    }
+  }  
 }
 
