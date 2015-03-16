9270627ff460b9a8cddc877d700962ad33196e37
Sam Swift <swift@betterment.com>
2015-03-16 12:24:40 -0400

    exporting one wod at a time to be more memory-conscious

--- export_data.R
+++ export_data.R
@@ -1,24 +1,23 @@
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### Export leaderboard and athlete data from DB to .RData
+###
+### Notes:
+###
+### Primary Creator(s): Sam Swift (samswift@gmail.com)
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 library(data.table)
 library(RMySQL)
 
 source("db_query.R")
 
-leaderboard.15 <- QueryDB("SELECT * FROM leaderboard WHERE year = 15;")
-save(leaderboard.15, file="data/leaderboard.15.RData")
-rm(leaderboard.15)
+ExportWod <- function(year, stage){
+  leaderboard <- QueryDB(paste0("SELECT * FROM leaderboard WHERE year = ", year, " AND stage = ",stage))
+  save(leaderboard, file=paste0("data/leaderboard.",year,".",stage,".RData"))
+}
 
-leaderboard.14 <- QueryDB("SELECT * FROM leaderboard WHERE year = 14;")
-save(leaderboard.14, file="data/leaderboard.14.RData")
-rm(leaderboard.14)
+ExportAthletes <- function(){
+  athletes <- QueryDB("SELECT * FROM athletes WHERE 1;")
+  save(athletes, file="data/athletes.RData")
+}
 
-leaderboard.13 <- QueryDB("SELECT * FROM leaderboard WHERE year = 13;")
-save(leaderboard.13, file="data/leaderboard.13.RData")
-rm(leaderboard.13)
-
-leaderboard.12 <- QueryDB("SELECT * FROM leaderboard WHERE year = 12;")
-save(leaderboard.12, file="data/leaderboard.12.RData")
-rm(leaderboard.12)
-
-athletes <- QueryDB("SELECT * FROM athletes WHERE 1;")
-save(athletes, file="data/athletes.RData")
-rm(athletes)
